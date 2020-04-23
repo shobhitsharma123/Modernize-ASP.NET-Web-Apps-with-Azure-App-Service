3e707bbbb1d477629fb4909b2abbb2b13e983cbd
shobhitsharma123 <63493884+shobhitsharma123@users.noreply.github.com>
2020-04-23 18:35:08 +0530

    13th commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modernize_ASP.NET_Web_Apps_with_Azure_App_Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
